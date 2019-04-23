14ca26a85763f2bc307c1d62e1dd2b21057a5948
MirajTariq <mirajtrq@gmail.com>
2019-04-23 18:35:39 +0500

    socket changes

--- socketServer.py
+++ socketServer.py
@@ -148,7 +148,7 @@ async def pushNotification(sid, data):
 #     print('Message: ' + str(data))
 
 @sio.on('searchperson')
-async def populateDict(sid, data):
+def populateDict(sid, data):
     print('session id: {' + str(sid) + '} request for person having id: {' + str(data['personid']) + '}')
 
     if str(data['personid']) not in personDict:
